19d0bb7c18fded1172873d0e3a02b42025196f3f
Blasty <rmzha2234@gmail.com>
2026-04-05 14:55:45 +1000

    Delete everything to prepare a shadcn NextJS app

--- app/ui/colors.ts
+++
@@ -1,3 +0,0 @@
-export const green = "#0fedbe";
-export const red = "#f63c6b";
-export const blue = "#00bddd";
--- app/ui/fonts.ts
+++
@@ -1,3 +0,0 @@
-import { Golos_Text } from "next/font/google";
-
-export const golosText = Golos_Text({ subsets: ["latin"] });
