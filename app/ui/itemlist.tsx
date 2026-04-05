3a80e7add16b961d085a16215ee03458123b272f
Blasty <rmzha2234@gmail.com>
2026-04-05 16:46:05 +1000

    Fix margin and padding issues

--- app/ui/itemlist.tsx
+++ app/ui/itemlist.tsx
@@ -10,14 +10,14 @@ import { Search } from "lucide-react"
 export default function ItemList() {
   return (
     <div className="flex flex-col fixed h-screen top-0 py-2 w-96 overflow-hidden">
-      <InputGroup className="mb-2 shrink-0">
+      <InputGroup className="ml-2 mb-2 shrink-0 max-w-93">
         <InputGroupInput placeholder="Search..." />
         <InputGroupAddon>
           <Search />
         </InputGroupAddon>
         <InputGroupAddon align={"inline-end"}></InputGroupAddon>
       </InputGroup>
-      <ScrollArea className="flex-1 min-h-0">
+      <ScrollArea className="ml-2 mr-1 flex-1 min-h-0">
         <ItemGroup className="gap-2">
         </ItemGroup>
       </ScrollArea>
